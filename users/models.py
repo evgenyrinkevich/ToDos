4dbe53ad552589f820394c387cfb6853cada95e4
evgenyrinkevich <rinkevich2000@gmail.com>
2022-02-24 10:41:35 +0300

    Lesson_1

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid4)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    username = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)

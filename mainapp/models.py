1cdf73e12821b428a689c6ecc344c8e2b7494e05
evgenyrinkevich <rinkevich2000@gmail.com>
2022-03-10 08:38:16 +0300

    Lesson_4  - fixed bugs from Lesson_3

--- mainapp/models.py
+++ mainapp/models.py
@@ -10,11 +10,15 @@ class Project(models.Model):
     github_url = models.URLField(max_length=128)
     users = models.ManyToManyField(User)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Todo(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid4)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=512, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todo')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
+from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import HyperlinkedModelSerializer
 
 from mainapp.models import Project, Todo
+from users.models import User
 from users.serializers import UserModelSerializer
 
 
+class UserListingSerializer(PrimaryKeyRelatedField, serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+    def to_representation(self, value):
+        return f'{value.get_username()}'
+
+
+class ProjectListingSerializer(PrimaryKeyRelatedField, serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['name']
+
+
 class ProjectModelSerializer(HyperlinkedModelSerializer):
-    users = serializers.StringRelatedField(many=True)
+    users = UserListingSerializer(many=True, queryset=User.objects.all())
 
     class Meta:
         model = Project
@@ -14,9 +31,17 @@ class ProjectModelSerializer(HyperlinkedModelSerializer):
 
 
 class TodoModelSerializer(HyperlinkedModelSerializer):
-    project = ProjectModelSerializer()
-    author = UserModelSerializer(read_only=True)
+    project = ProjectListingSerializer(queryset=Project.objects.all())
+    author = User.objects.all()
 
     class Meta:
         model = Todo
-        fields = ('project', 'author', 'created_at', 'updated_at', 'is_active', 'url')
+        fields = ('project', 'author', 'text', 'created_at', 'updated_at', 'is_active', 'url')
+
+    def create(self, validated_data):
+        project_data = validated_data.pop('project')
+        Project.objects.create(users=[self.author], **project_data)
+        todo = Todo.objects.create(**validated_data)
+
+        return todo
+
